52db15fdc57b2426eaee714831a7cd27c06aa3e8
Krystian Mączeński <Krystian.Maczenski@microsoft.wsei.edu.pl>
2022-07-02 16:08:03 +0200

    old version

--- ProjektPrzychodnia/Pacjent.cs
+++ ProjektPrzychodnia/Pacjent.cs
@@ -14,7 +14,7 @@ namespace ProjektPrzychodnia
     using System.ComponentModel;
     using System.Text.RegularExpressions;
 
-    public partial class Pacjent : IDataErrorInfo
+    public partial class Pacjent 
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Pacjent()
